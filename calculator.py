31ed7482092b6506eee55a27353dc48b4de17bba
Arnav Srivastava <arnavsrivastava@Arnavs-MacBook-Air.local>
2022-10-29 17:49:34 -0700

    calculator

---
+++ calculator.py
@@ -0,0 +1,10 @@
+def add(i, j):
+    return i + j
+
+# test cases
+result1 = add(10, 20)
+print(result1)
+if result1 == 30:
+    print("add test case passed")
+else:
+    print("add test case failed")
